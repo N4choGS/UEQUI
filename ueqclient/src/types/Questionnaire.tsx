6cbf5b03a8a19eaae5b29460b32dbae0aba71c90
Carlos Gómez Segura (from Dev Box) <cgomezsegura@microsoft.com>
2023-09-08 21:02:39 -0600

    Update on the type quest

--- ueqclient/src/types/Questionnaire.tsx
+++ ueqclient/src/types/Questionnaire.tsx
@@ -7,6 +7,44 @@ export interface Questionnaire {
     projectId : number;
     project? : Catalog;
     createdDate :  Date ;
+    attractiveness: {
+        annoying_enjoyable:number;
+        bad_good:number;
+        unlikable_pleasing:number;
+        unpleasant_pleasant:number;
+        unattractive_attractive:number;
+        unfriendly_friendly:number;
+    },
+    novelty: {
+        dull_creative:number;
+        usual_leadingEdge:number;
+        conventional_inventive:number;
+        conservative_innovative:number;
+    },
+    efficiency: {
+        slow_fast:number;
+        inefficient_efficient:number;
+        impractical_practical:number;
+        cluttered_organized:number;
+    },
+    dependability: {
+        unpredictable_predictable:number;
+        notSecure_secure:number;
+        doesNotMeetExpectations_meetsExpectations:number;
+        obstructive_supportive:number;
+    },
+    perspecuity: {
+        unpredictable_predictable:number;
+        difficultToLearn_easyToLearn:number;
+        complicated_easy:number;
+        confusing_clear:number;
+    },
+    stimulation: {
+        inferior_valuable:number;
+        boring_exciting:number;
+        notInteresting_interesting:number;
+        motivating_demotivating:number;
+    },
     annoying_enjoyable : number;
     notUnderstandable_understandable : number;
     dull_creative : number;
